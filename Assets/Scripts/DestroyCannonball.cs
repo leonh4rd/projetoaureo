b2452a07cdfdb3b478063703a5f04e094884bce9
Leonardo (Notebook) <lnrd.ferreira@hotmail.com>
2019-07-07 20:39:11 -0300

    Add cannonball controls

---
+++ Assets/Scripts/DestroyCannonball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCannonball : MonoBehaviour
+{
+    private float seconds;
+    private float counter;
+
+    void Start()
+    {
+        seconds = 5f;
+        counter = 0f;
+    }
+
+    void Update()
+    {
+        counter += Time.deltaTime;
+        if(counter >= seconds)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ShootCannonball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootCannonball : MonoBehaviour
+{
+    [SerializeField]
+    Transform cannonballPrefab;
+    [SerializeField]
+    float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Transform cannonball = Instantiate(cannonballPrefab, transform.position, Quaternion.identity);
+            Vector2 velocity = transform.right * speed;
+            cannonball.GetComponent<Rigidbody2D>().AddForce(velocity, ForceMode2D.Impulse);
+        }
+    }
+}
